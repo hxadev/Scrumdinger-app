773ee921ff39ff56b58817dcf40c7f0f004ba658
Alfonso Hernandez <alfonso_h_x@hotmail.com>
2024-02-03 12:38:34 -0600

    Creating a card view, adding label, padding, text, accesibility label and text

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -7,6 +7,35 @@
 
 import Foundation
 
-struct {
+struct DailyScrum{
+    var title: String
+    var attendes: [String]
+    var lengthInMinutes: Int
+    var theme: Theme
     
+}
+
+extension DailyScrum{
+    static let sampleData: [DailyScrum] =
+    [
+        DailyScrum(
+            title: "Design",
+            attendes: ["Kathy","Alfonso","Anuar","Gloaria"],
+            lengthInMinutes: 10,
+            theme: .yellow
+        ),
+        DailyScrum(
+            title: "App Dev",
+            attendes: ["Krathy","Anuar","Ron"],
+            lengthInMinutes: 15,
+            theme: .purple
+        ),
+        DailyScrum(
+            title: "Web Dev",
+            attendes: ["Krathy","Anuar","Ron"],
+            lengthInMinutes: 15,
+            theme: .poppy
+        )
+        
+    ]
 }
--- Scrumdinger/TrailingIcon.swift
+++ Scrumdinger/TrailingIcon.swift
@@ -5,4 +5,19 @@
 //  Created by Alfonso on 03/02/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle{
+    func makeBody(configuration: Configuration) -> some View {
+        HStack{
+            configuration.title
+            configuration.icon
+            
+        }
+    }
+}
+
+extension LabelStyle where Self==TrailingIconLabelStyle{
+    static var trailingIcon: Self{ Self()}
+    
+}
